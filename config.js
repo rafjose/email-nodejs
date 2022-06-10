236df16142832a0ecb434d46b34d402c1d9f21fc
Rafael José da Silva <rafael.jose.silva.dev@gmail.com>
2022-06-10 11:37:42 -0300

    Conclusão do projeto envio de email

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    // Theme
+    theme: "lightBlue",
+
+    // Email notifier account (sender)
+    host: "smtp.mailtrap.io", // Sender email smtp
+    port: "465", // Sender email port
+    user: "testedeaplicativo4@gmail.com", // Sender email username
+    pass: "ojrsamoutrtxgipm", // Sender email password
+
+    // Your email to receive notification (receiver)  
+    from: '"Contact Me" <noreply@example.com>', // Sender email address
+    to: 'testedeaplicativo4@gmail.com', // Your email address
+    subject: 'Contact Us', // Subject
+};
